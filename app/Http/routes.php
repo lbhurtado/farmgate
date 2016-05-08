d18a510107e5e68df6d2c4671d84eb77c7d42e87
Lester Hurtado <lester@applester.co>
2016-05-08 09:02:21 +0800

    Modify routes re: Route collection error 2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,12 +23,12 @@ Route::resource('groups', 'GroupsController');
 Route::post('sms/{from}/{to}/{message}', 'SMSController@post');
 
 Route::post('sun', function(){
-    $mobile = Request::get('from');
-    $message = "fwd: " . Request::get('msg');
-    
-    SMS::queue($message, [], function($sms) use ($mobile, $message) {
-        $sms->to($mobile);
-    });
+//    $mobile = Request::get('from');
+//    $message = "fwd: " . Request::get('msg');
+//
+//    SMS::queue($message, [], function($sms) use ($mobile, $message) {
+//        $sms->to($mobile);
+//    });
 
     return Request::all();
 });
